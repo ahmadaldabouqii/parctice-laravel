491173d555fb31f514fdecaece1025e24c70cb7a
ahmadaldabouqii <aldabouqiahmad@gmail.com>
2022-03-27 09:58:49 +0300

    replace normal menu with sidebar menu | remove repetitve code

--- task-1/resources/views/sub-categories.blade.php
+++ task-1/resources/views/sub-categories.blade.php
@@ -1,8 +1,8 @@
 @include('layouts.table-head')
 <table class="table table-hover">
     <thead>
-    <th>Sub Category Name</th>
-    <th>Is Active</th>
+        <th>Sub Category Name</th>
+        <th>Is Active</th>
     </thead>
     <tbody>
         @foreach($sub_categories as $subCategory)
